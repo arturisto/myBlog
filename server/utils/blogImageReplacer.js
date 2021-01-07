7c8b876dde4ac274b1d1e6cbd2fad657e015d4bf
Arthurgit cinfig --global user.email arthurr.ie@gmail.com <arthurr.ie@gmail.com>
2021-01-07 17:00:37 +0200

    image replacer

--- server/utils/blogImageReplacer.js
+++ server/utils/blogImageReplacer.js
@@ -1,42 +1,61 @@
-
 const fs = require('fs');
 const S3FS = require('s3fs')
 const aws = require("aws-sdk");
 
 
 const tempFolder = __dirname+"\\temp";
+const bucketUrl = "https://mrandmrseatmedia.s3.us-east-2.amazonaws.com/"
+const localHostPath = "http://localhost:8000\\MrMsEat_App\\mr-mrs-app\\server\\utils\\temp\\"
 
 const imageReplacer =async ( body, title) => {
     const bucketName = process.env['BUCKET_NAME']
-    let newEntry = '';
-    
+    let newEntry = body;
+    const trimedTitle =  title.replace(/\s/g, '');
     const s3fsImpl = new S3FS(bucketName, {
         accessKeyId: process.env["AWS_ACCESS_KEY_ID"],
         secretAccessKey: process.env["AWS_SECRET_KEY"],
-        region: process.env['AWS_REGION']
+        region: process.env['AWS_REGION'],
     });
-  
+    
     for await (const file of fs.readdirSync(tempFolder)){
+
             let filePath = tempFolder+"\\"+file;
-            console.log("file",file) 
-            console.log("start of upload",filePath ) 
-            fs.readFile(filePath, async (err, fileToUpload) =>{
+
+
+            // eslint-disable-next-line no-loop-func
+            await fs.readFile(filePath, async (err, fileToUpload, options) =>{
                     if (err){
                         console.log("err",err)
                     }
                     else{
-
                     var stream = fs.createReadStream(filePath);
-                    const fileName = bucketName+"\\"+title+ "\\"+file  
-                    await s3fsImpl.writeFile(fileName, stream)
-                    .then(function () {
-                            console.log('It\'s saved!');                                      
+                    const fileName = bucketName+"/"+trimedTitle+ "/"+file
+                    const options = {
+                        ACL:'public-read'
+                    }  
+                    await s3fsImpl.writeFile(fileName, stream, options)
+                    .then(function (res) {
+                            let newUrl =  bucketUrl+trimedTitle+"/"+file;
+                            // console.log("old path",localHostPath+file)
+                            // console.log(newUrl)
+                            // console.log(newEntry)
+
+                            let oldPath =localHostPath+file
+                                console.log("old path",oldPath)
+                            console.log(newUrl)
+                            console.log(newEntry)
+
+                            newEntry = newEntry.replace(oldPath,newUrl); 
+                            console.log("new",newEntry)                    
                             });
                         }
     
                                 
             })    
-        }
+        
+    }
+        console.log("return",newEntry)
+        return newEntry
 }
 
 
