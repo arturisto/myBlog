8255e073961ff8eb538e926bb57a93589140cd44
arturisto <arthurr.ie@gmail.com>
2021-04-05 21:41:32 +0300

    heroku deploy changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Routes from "./router";
 import Footer from "./containers/Footer/Footer";
 import Navbar from "./containers/NavBar/navbar";
 function App() {
+  console.log("app");
   return (
     <div className="App">
       <Navbar />
