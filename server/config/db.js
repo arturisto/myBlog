657069403824e109f6ffa23815be3b98b644ebfb
arturisto <arthurr.ie@gmail.com>
2021-04-06 08:35:59 +0300

    heroku deploy changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,20 @@
 const Sequelize = require("sequelize");
 console.log("hi db");
-const db = new Sequelize(process.env["DATABASE_URL"], {
-  dialect: "postgres",
-  protocol: "postgres",
-});
+const db = new Sequelize(
+  process.env["DATABASE_NAME"],
+  process.env["DATABASE_USERNAME"],
+  process.env["DATABASE_PASSWORD"],
+  {
+    host: process.env["DATABASE_HOST"],
+    dialect: "postgres",
+    protocol: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 module.exports = db;
