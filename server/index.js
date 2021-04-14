a5ec10309d76016668f634fde56dd9456226dfbb
arturisto <arthurr.ie@gmail.com>
2021-04-14 18:24:35 +0300

    heroku db testings

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const jwt = require("jsonwebtoken");
 app.use("/", routes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log("server on port ", PORT);
 });
 app.use(express.static(path.join(__dirname, "../", "client/build")));
