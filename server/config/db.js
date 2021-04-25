a993b115d19b61c4d0e4fe1edd865758bcee85af
arturisto <arthurr.ie@gmail.com>
2021-04-25 15:52:13 +0300

    amplify build test

--- server/config/db.js
+++ server/config/db.js
@@ -4,12 +4,14 @@ console.log(
   process.env["DATABASE_NAME"],
   process.env["DATABASE_USERNAME"],
   process.env["DATABASE_PASSWORD"],
-  process.env["DATABASE_HOST"]
+  process.env["DATABASE_HOST"],
+  process.env["PGSSLMODE"]
 );
 const ssl = {
   ssl: process.env["PGSSLMODE"],
 };
 const options = process.env["NODE_ENV"] === "DEV" ? {} : ssl;
+console.log(options);
 const db = new Sequelize(
   process.env["DATABASE_NAME"],
   process.env["DATABASE_USERNAME"],
