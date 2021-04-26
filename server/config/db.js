79c8786753abfaef68815b41e63460f39ddd8b2c
arturisto <arthurr.ie@gmail.com>
2021-04-26 17:25:11 +0300

    amplify build test

--- server/config/db.js
+++ server/config/db.js
@@ -22,12 +22,15 @@ const db = new Sequelize(
     logging: console.log,
     timeout: 6000,
     pool: {
-      max: 5,
+      max: 15,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
-    dialectOptions: options,
+    dialectOptions: {
+      ssl: "Amazon RDS",
+      onnectTimeout: 60000,
+    },
   }
 );
 
