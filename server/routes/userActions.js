a93e4ce386d4984a270e52dd887239554b9ac9ca
arturisto <arthurr.ie@gmail.com>
2021-04-27 08:31:37 +0300

    amplify build test

--- server/routes/userActions.js
+++ server/routes/userActions.js
@@ -1,7 +1,7 @@
 //libraries
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 const fs = require("fs");
 
